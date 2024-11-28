306148ead4f786bc067b9c0869241bcd0bb2fa72
Anand Vardhan <133073364+Nandostream11@users.noreply.github.com>
2024-11-28 08:34:51 +0530

    Delete src/install directory

--- src/install/superbot_description/share/superbot_description/cmake/superbot_descriptionConfig-version.cmake
+++
@@ -1,1 +0,0 @@
-/home/parth/Superbot_ws/src/build/superbot_description/ament_cmake_core/superbot_descriptionConfig-version.cmake
--- src/install/superbot_description/share/superbot_description/local_setup.zsh
+++
@@ -1,1 +0,0 @@
-/home/parth/Superbot_ws/src/build/superbot_description/ament_cmake_environment_hooks/local_setup.zsh
