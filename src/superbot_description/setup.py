e743bf34cbbc26e4a964324fc71b4b5ff8e0ab19
Anand Vardhan <anandvk113@gmail.com>
2024-11-30 22:23:00 +0530

    Setup.py

--- src/superbot_description/setup.py
+++ src/superbot_description/setup.py
@@ -8,19 +8,21 @@ setup(
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     data_files=[
-        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', 
+            ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='anandv',
-    maintainer_email='anandv@todo.todo',
+    maintainer_email='anandvk113@gmail.com',
     description='Superbot Description Package',
     license='Apache-2.0',
+    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
             'robot_listener = superbot_description.ros_nodes.robot_listener:main',
-            'data_publisher = superbot_description.ros_nodes.data_publisher:main'
+            'data_publisher = superbot_description.ros_nodes.data_publisher:main',
         ],
     },
 )
